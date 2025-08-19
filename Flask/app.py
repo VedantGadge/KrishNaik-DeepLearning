d8faf7ef1f1b82e8065460ff21e5c3dbf31f5e35
VedantGadge <vedant.gadgegsis@gmail.com>
2025-08-20 00:33:39 +0530

    .

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
 
 '''
 This will create a an instance of the Flask class,
@@ -14,6 +14,26 @@ def welcome():
 def index():
     return render_template('index.html') # This will find the folder named 'templates' folder and find the given file
 
+@app.route('/form',methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name = request.form['name']
+        return f'<h1>Welcome {name}!<h1>'
+    return render_template('form.html')
+
+###Jinja 2 template
+
+@app.route('/success/<int:score>') ## we are giving some rule to the parameter , this is known as variable ruling
+def success(score):
+    res =""
+    if score>=50:
+        res = "PASSED"
+    else:
+        res = "FAILED"
+    
+    return render_template('results.html',results = res)
+        
+        
 
 
 # Entry point of this application (app.py)
